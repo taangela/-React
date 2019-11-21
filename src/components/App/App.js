f4dd1faf49e48aea76873b5e64940c44b18bd494
Marta Koperek <makoperek@gmail.com>
2019-11-21 13:37:43 +0100

    Fix: Hero is now a function component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js';
 
-
 class App extends React.Component {
   render() {
     return (
@@ -10,7 +9,6 @@ class App extends React.Component {
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!!</h2>
 	    <List/>
-        
       </main>
     )
   }

c31e41a2dd8f2df58a20473b80f8846fb9a83de0
Marta Koperek <makoperek@gmail.com>
2019-11-21 23:07:16 +0100

    Prettier code

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import styles from './Hero.scss';
-import PropTypes from 'prop-types';
-
+import React from "react";
+import styles from "./Hero.scss";
+import PropTypes from "prop-types";
 
 const Hero = props => (
-      <header className={styles.component}>
-		<h2 className={styles.title}>{props.titleText}</h2>
-        <img className={styles.image} src= {props.imgImage}></img>
-      </header>
-    );
+  <header className={styles.component}>
+    <h2 className={styles.title}>{props.titleText}</h2>
+    <img className={styles.image} src={props.imgImage}></img>
+  </header>
+);
 
 Hero.propTypes = {
   titleText: PropTypes.node.isRequired,

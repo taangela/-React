c8c04e21151a58fb1c5fed6bbb4d010c3ff43e80
Marta Koperek <makoperek@gmail.com>
2019-11-21 13:08:52 +0100

    Fix: wrong Hero nesting

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js';
-import Hero from '../Hero/Hero.js';
 
 
 class App extends React.Component {
@@ -10,7 +9,6 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!!</h2>
-        <Hero/>
 	    <List/>
         
       </main>

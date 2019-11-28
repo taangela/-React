e3a90d380551047c64b039d20e3456c838282eab
Marta Koperek <makoperek@gmail.com>
2019-11-28 23:37:22 +0100

    Add search box

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,22 +1,23 @@
 // selectors
 export const getSearchString = ({searchString}) => searchString;
 export const countAllCards = ({cards}) => cards.length;
-
 export const countVisibleCards =({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
-// action name creator
 
+// action name creator
+const reducerName = 'changeSearchString';
+const createActionName = name => `app/${reducerName}/${name}`;
 // actions types
+export const CHANGE = createActionName('CHANGE');
 
 // action creators
+export const createAction_changeSearchString = payload => ({ payload, type: CHANGE });
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case CHANGE:
+      return action.payload;
     default:
       return statePart;
   }
 }
-
-
-
-export const getCardsForColumn = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);

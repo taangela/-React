c80184c061539830a5c2f13316cc8f8e4ccf199b
Marta Koperek <makoperek@gmail.com>
2019-12-05 12:25:21 +0100

    Bug: Animated Switch not working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,16 +3,22 @@ import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
 import FAQ from '../FAQ/FAQ';
 import MainLayout from '../MainLayout/MainLayout';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+import {AnimatedSwitch} from 'react-router-transition';
 
 const App=() => (
   <BrowserRouter>
     <MainLayout>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/info' component={Info} />
-        <Route exact path='/FAQ' component={FAQ} />
-      </Switch>
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className="switch-wrapper"
+      >
+        <Route exact path="/" component={Home} />
+        <Route exact path="/info" component={Info} />
+        <Route exact path="/FAQ" component={FAQ} />
+      </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
 );
